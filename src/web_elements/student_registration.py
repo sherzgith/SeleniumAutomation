21ea3eee63387538aa2f85e6984c0fec9b7b23fe
sherzgith <riskaliyevsherzod@gmail.com>
2023-09-17 16:43:04 -0700

    update project , added finding elements by, link and partial link

--- src/web_elements/student_registration.py
+++ src/web_elements/student_registration.py
@@ -38,17 +38,41 @@ print('maximizing the browser window')
 driver.maximize_window()
 time.sleep(1)
 
+# Sample of different Locators:
+driver.get('https://demoqa.com/links')
+time.sleep(2)
+driver.find_element(By.LINK_TEXT, 'Created').click()
+time.sleep(5)
+print('clicking the No Content link partial part :conte')
+driver.find_element(By.PARTIAL_LINK_TEXT, 'Conte').click()
+time.sleep(10)
+
+
 print("# Open the student registration form.")
 driver.get(host)
 print("Zoom the page to 55 %")
 driver.execute_script("document.body.style.zoom='55%'")
 
-print("# Enter name, last name, email,")
+print("# Enter name...")
 fname_input = driver.find_element(By.ID, 'firstName')
 fname_input.send_keys('djvdkvddklv')
 time.sleep(2)
 fname_input.clear()
 fname_input.send_keys(fname)
+
+print("# Enter last name...")
+driver.find_element(By.ID, 'lastName').send_keys(lname)
+time.sleep(2)
+
+# or
+# lname_input = driver.find_element(By.ID, 'lastName')
+# lname_input.send_keys('flglammvdvm')
+# time.sleep(2)
+# lname_input.clear()
+# lname_input.send_keys(lname)
+print("# Enter the email...")
+driver.find_element(By.ID, 'userEmail').send_keys(email)
+time.sleep(5)
 
 
 # Enter Mobile, Subjects, current address
@@ -57,4 +81,6 @@ fname_input.send_keys(fname)
 # Select Checkboxes ( multi select)
 # picture upload ( input, type=file)
 # Select State(first), then select City (Click the visible container)
+print('*************COMPLETED, Closing the BROWSER!!!!')
+driver.close()
 
